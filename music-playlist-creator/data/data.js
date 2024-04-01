85c67813eb3c00e2856520917ecb987279908bcb
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2024-04-01 17:57:23 -0500

    Update data.js

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -3,7 +3,7 @@ let data = { "playlists": [
       "playlistID": 0,
       "playlist_name": "Late Night Drive",
       "playlist_creator": "Melody Maven",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/36/200",
       "likeCount": 0,
       "songs": [
         {
@@ -11,7 +11,7 @@ let data = { "playlists": [
           "title": "Nightcall",
           "artist": "Kavinsky",
           "album": "Nightcall",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/43/200",
           "duration": "4:18"
         },
         {
@@ -19,7 +19,7 @@ let data = { "playlists": [
           "title": "Drive",
           "artist": "The Cars",
           "album": "Heartbeat City",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/45/200",
           "duration": "3:55"
         },
         {
@@ -27,7 +27,7 @@ let data = { "playlists": [
           "title": "Cruisin'",
           "artist": "Smokey Robinson",
           "album": "One Heartbeat",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/88/200",
           "duration": "4:30"
         },
         {
@@ -35,7 +35,7 @@ let data = { "playlists": [
           "title": "Night Moves",
           "artist": "Bob Seger",
           "album": "Night Moves",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/84/200",
           "duration": "5:25"
         },
         {
@@ -43,7 +43,7 @@ let data = { "playlists": [
           "title": "Moonlight Drive",
           "artist": "The Doors",
           "album": "Strange Days",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/122/200",
           "duration": "3:04"
         }
       ]
@@ -52,7 +52,7 @@ let data = { "playlists": [
       "playlistID": 1,
       "playlist_name": "That Summer '16 Feeling",
       "playlist_creator": "Groove Guardian",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/117/200",
       "likeCount": 0,
       "songs": [
         {
@@ -60,7 +60,7 @@ let data = { "playlists": [
           "title": "One Dance",
           "artist": "Drake",
           "album": "Views",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/21/200,
           "duration": "2:53"
         },
         {
@@ -68,7 +68,7 @@ let data = { "playlists": [
           "title": "Cheap Thrills",
           "artist": "Sia",
           "album": "This Is Acting",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/56/200",
           "duration": "3:31"
         },
         {
@@ -76,7 +76,7 @@ let data = { "playlists": [
           "title": "Don't Let Me Down",
           "artist": "The Chainsmokers ft. Daya",
           "album": "Collage",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/145/200",
           "duration": "3:28"
         },
         {
@@ -84,7 +84,7 @@ let data = { "playlists": [
           "title": "This Is What You Came For",
           "artist": "Calvin Harris ft. Rihanna",
           "album": "This Is What You Came For",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/160/200",
           "duration": "3:42"
         },
         {
@@ -92,7 +92,7 @@ let data = { "playlists": [
           "title": "Ride",
           "artist": "Twenty One Pilots",
           "album": "Blurryface",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/133/200",
           "duration": "3:34"
         }
       ]
@@ -101,7 +101,7 @@ let data = { "playlists": [
       "playlistID": 2,
       "playlist_name": "Workout Beats",
       "playlist_creator": "Harmony Herald",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/1077/200",
       "likeCount": 0,
       "songs": [
         {
@@ -109,7 +109,7 @@ let data = { "playlists": [
           "title": "Eye of the Tiger",
           "artist": "Survivor",
           "album": "Eye of the Tiger",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/593/200",
           "duration": "4:05"
         },
         {
@@ -117,7 +117,7 @@ let data = { "playlists": [
           "title": "Stronger",
           "artist": "Kanye West",
           "album": "Graduation",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/169/200",
           "duration": "5:12"
         },
         {
@@ -125,7 +125,7 @@ let data = { "playlists": [
           "title": "Lose Yourself",
           "artist": "Eminem",
           "album": "8 Mile Soundtrack",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/171/200",
           "duration": "5:26"
         },
         {
@@ -133,7 +133,7 @@ let data = { "playlists": [
           "title": "Uptown Funk",
           "artist": "Mark Ronson ft. Bruno Mars",
           "album": "Uptown Special",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/274/200",
           "duration": "4:30"
         },
         {
@@ -141,7 +141,7 @@ let data = { "playlists": [
           "title": "Can't Stop the Feeling!",
           "artist": "Justin Timberlake",
           "album": "Trolls (Original Motion Picture Soundtrack)",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/304/200",
           "duration": "3:56"
         }
       ]
@@ -150,7 +150,7 @@ let data = { "playlists": [
       "playlistID": 3,
       "playlist_name": "Road Trip Anthems",
       "playlist_creator": "Playlist Pioneer",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/314/200",
       "likeCount": 0,
       "songs": [
         {
@@ -158,7 +158,7 @@ let data = { "playlists": [
           "title": "Life Is a Highway",
           "artist": "Tom Cochrane",
           "album": "Mad Mad World",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/352/200",
           "duration": "4:26"
         },
         {
@@ -166,7 +166,7 @@ let data = { "playlists": [
           "title": "Born to Run",
           "artist": "Bruce Springsteen",
           "album": "Born to Run",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/361/200",
           "duration": "4:30"
         },
         {
@@ -174,7 +174,7 @@ let data = { "playlists": [
           "title": "Take It Easy",
           "artist": "Eagles",
           "album": "Eagles",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/365/200",
           "duration": "3:31"
         },
         {
@@ -182,7 +182,7 @@ let data = { "playlists": [
           "title": "Route 66",
           "artist": "Chuck Berry",
           "album": "Get Your Kicks on Route 66",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/371/200",
           "duration": "2:53"
         },
         {
@@ -190,7 +190,7 @@ let data = { "playlists": [
           "title": "On the Road Again",
           "artist": "Willie Nelson",
           "album": "Honeysuckle Rose",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/382/200",
           "duration": "2:33"
         }
       ]
@@ -199,7 +199,7 @@ let data = { "playlists": [
       "playlistID": 4,
       "playlist_name": "Boss Chick Energy",
       "playlist_creator": "Tempo Tactician",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/449/200",
       "likeCount": 0,
       "songs": [
         {
@@ -207,7 +207,7 @@ let data = { "playlists": [
           "title": "Run the World (Girls)",
           "artist": "Beyoncé",
           "album": "4",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/550/200",
           "duration": "3:56"
         },
         {
@@ -215,7 +215,7 @@ let data = { "playlists": [
           "title": "I'm Every Woman",
           "artist": "Chaka Khan",
           "album": "Chaka",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/646/200",
           "duration": "4:07"
         },
         {
@@ -223,7 +223,7 @@ let data = { "playlists": [
           "title": "Independent Women, Pt. 1",
           "artist": "Destiny's Child",
           "album": "Survivor",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/660/200",
           "duration": "3:41"
         },
         {
@@ -231,7 +231,7 @@ let data = { "playlists": [
           "title": "Confident",
           "artist": "Demi Lovato",
           "album": "Confident",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/680/200",
           "duration": "3:25"
         },
         {
@@ -239,7 +239,7 @@ let data = { "playlists": [
           "title": "Feeling Myself",
           "artist": "Nicki Minaj ft. Beyoncé",
           "album": "The Pinkprint",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/822/200",
           "duration": "3:57"
         }
       ]
@@ -248,7 +248,7 @@ let data = { "playlists": [
       "playlistID": 5,
       "playlist_name": "Rainy Nights",
       "playlist_creator": "Sonic Sage",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/115/200",
       "likeCount": 0,
       "songs": [
         {
@@ -256,7 +256,7 @@ let data = { "playlists": [
           "title": "Purple Rain",
           "artist": "Prince",
           "album": "Purple Rain",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/123/200",
           "duration": "8:41"
         },
         {
@@ -264,7 +264,7 @@ let data = { "playlists": [
           "title": "November Rain",
           "artist": "Guns N' Roses",
           "album": "Use Your Illusion I",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/171/200",
           "duration": "8:57"
         },
         {
@@ -272,7 +272,7 @@ let data = { "playlists": [
           "title": "Riders on the Storm",
           "artist": "The Doors",
           "album": "L.A. Woman",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/172/200",
           "duration": "7:14"
         },
         {
@@ -280,7 +280,7 @@ let data = { "playlists": [
           "title": "I Can't Stand the Rain",
           "artist": "Ann Peebles",
           "album": "I Can't Stand the Rain",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/178/200",
           "duration": "2:31"
         },
         {
@@ -288,7 +288,7 @@ let data = { "playlists": [
           "title": "Set Fire to the Rain",
           "artist": "Adele",
           "album": "21",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/680/200",
           "duration": "4:02"
         }
       ]
@@ -297,7 +297,7 @@ let data = { "playlists": [
       "playlistID": 6,
       "playlist_name": "90's R&B",
       "playlist_creator": "Vinyl Virtuoso",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/815/200",
       "likeCount": 0,
       "songs": [
         {
@@ -305,7 +305,7 @@ let data = { "playlists": [
           "title": "No Diggity",
           "artist": "Blackstreet ft. Dr. Dre",
           "album": "Another Level",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/856/200",
           "duration": "5:06"
         },
         {
@@ -313,7 +313,7 @@ let data = { "playlists": [
           "title": "Waterfalls",
           "artist": "TLC",
           "album": "CrazySexyCool",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/871/200",
           "duration": "4:39"
         },
         {
@@ -321,7 +321,7 @@ let data = { "playlists": [
           "title": "Creep",
           "artist": "TLC",
           "album": "CrazySexyCool",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/891/200",
           "duration": "4:29"
         },
         {
@@ -329,7 +329,7 @@ let data = { "playlists": [
           "title": "On Bended Knee",
           "artist": "Boyz II Men",
           "album": "II",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/1013/200",
           "duration": "4:05"
         },
         {
@@ -337,7 +337,7 @@ let data = { "playlists": [
           "title": "Fantasy",
           "artist": "Mariah Carey",
           "album": "Daydream",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/1014/200",
           "duration": "4:04"
         }
       ]
@@ -346,7 +346,7 @@ let data = { "playlists": [
       "playlistID": 7,
       "playlist_name": "Nothing But The Classics",
       "playlist_creator": "Echo Enthusiast",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/22/200",
       "likeCount": 0,
       "songs": [
         {
@@ -354,7 +354,7 @@ let data = { "playlists": [
           "title": "Bohemian Rhapsody",
           "artist": "Queen",
           "album": "A Night at the Opera",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/39/200",
           "duration": "5:55"
         },
         {
@@ -362,7 +362,7 @@ let data = { "playlists": [
           "title": "Hotel California",
           "artist": "Eagles",
           "album": "Hotel California",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/57/200",
           "duration": "6:30"
         },
         {
@@ -370,7 +370,7 @@ let data = { "playlists": [
           "title": "Stairway to Heaven",
           "artist": "Led Zeppelin",
           "album": "Led Zeppelin IV",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/221/200",
           "duration": "8:02"
         },
         {
@@ -378,7 +378,7 @@ let data = { "playlists": [
           "title": "Sweet Child o' Mine",
           "artist": "Guns N' Roses",
           "album": "Appetite for Destruction",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/822/200",
           "duration": "5:55"
         },
         {
@@ -386,7 +386,7 @@ let data = { "playlists": [
           "title": "Imagine",
           "artist": "John Lennon",
           "album": "Imagine",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/823/200",
           "duration": "3:04"
         }
       ]
@@ -395,7 +395,7 @@ let data = { "playlists": [
       "playlistID": 8,
       "playlist_name": "Ohh That Baseline",
       "playlist_creator": "Bassline Buddy",
-      "playlist_art": "assets/img/playlist.png",
+      "playlist_art": "https://picsum.photos/id/836/200",
       "likeCount": 0,
       "songs": [
         {
@@ -403,7 +403,7 @@ let data = { "playlists": [
           "title": "Another One Bites the Dust",
           "artist": "Queen",
           "album": "The Game",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/839/200",
           "duration": "3:36"
         },
         {
@@ -411,7 +411,7 @@ let data = { "playlists": [
           "title": "Billie Jean",
           "artist": "Michael Jackson",
           "album": "Thriller",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/903/200",
           "duration": "4:54"
         },
         {
@@ -419,7 +419,7 @@ let data = { "playlists": [
           "title": "Superstition",
           "artist": "Stevie Wonder",
           "album": "Talking Book",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/978/200",
           "duration": "4:26"
         },
         {
@@ -427,7 +427,7 @@ let data = { "playlists": [
           "title": "Smooth Criminal",
           "artist": "Michael Jackson",
           "album": "Bad",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/1004/200",
           "duration": "4:17"
         },
         {
@@ -435,7 +435,7 @@ let data = { "playlists": [
           "title": "Money for Nothing",
           "artist": "Dire Straits",
           "album": "Brothers in Arms",
-          "cover_art": "assets/img/song.png",
+          "cover_art": "https://picsum.photos/id/1025/200",
           "duration": "8:26"
         }
       ]
