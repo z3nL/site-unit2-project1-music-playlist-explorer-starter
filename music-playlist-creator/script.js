c5da8264cd11ef88d4b708b698f157ecc56f1afd
Zen Lambertus <zenl@meta.com>
2025-06-04 10:23:00 -0700

    finished milestone 1

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,16 @@
+const modal = document.getElementById("playlistModal");
+const close = document.getElementsByClassName("close");
 
+function toggleModal() {
+    if (modal.style.display === "flex") {
+        modal.style.display = "none";
+        return;
+    }
+    modal.style.display = "flex";
+}
+
+window.onclick = function(event) {
+   if (event.target === modal) {
+      modal.style.display = "none";
+   }
+}
